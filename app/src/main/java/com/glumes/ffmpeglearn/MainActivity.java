11bb78b96ef6cc9001af7191294f49d3774791f0
glumes <zhaoying9402@gmail.com>
2017-05-15 20:39:53 +0800

    add so library

--- app/src/main/java/com/glumes/ffmpeglearn/MainActivity.java
+++ app/src/main/java/com/glumes/ffmpeglearn/MainActivity.java
@@ -8,7 +8,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+//        System.loadLibrary("native-lib");
+//        System.loadLibrary("avcodec-57_lib");
+
     }
 
     @Override
@@ -18,12 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        tv.setText(stringFromJNI());
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+//    public native String stringFromJNI();
 }
